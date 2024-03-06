74124da55386f7ed9c3b8dd9366a790b8655b91c
8888k666 <162081572+8888k666@users.noreply.github.com>
2024-03-06 16:29:17 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,5 +6,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(bytes(self.client_address[0].encode('utf-8')))
+        # self.wfile.write('Hello, world!'.encode('utf-8'))
         return
